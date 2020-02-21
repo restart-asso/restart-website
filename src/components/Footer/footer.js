789407d647ed1cec27f3a5679b7d93d5d13450d9
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2020-02-22 00:16:04 +0100

    Add footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,48 @@
+import React from "react"
+// A more holistic way to add the icons throughout the app 
+// https://github.com/FortAwesome/react-fontawesome#build-a-library-to-reference-icons-throughout-your-app-more-conveniently
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import styles from "./footer.module.css"
+
+export default () => (
+    <footer> 
+        <div className={styles.container}>
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src="/img/restart-logo.png" alt="Re:Start logo"/>
+            </div>
+            <div className={styles.separator}></div>
+            <div className={styles.infoContainer}>
+                <span>Contact</span>
+                <span>
+                    <a href="mailto:hello@restart-asso.org">
+                        hello@restart-asso.org
+                    </a>
+                </span>
+                <span className={styles.socialMedia}>
+                    <a href="https://twitter.com/restartAsso">
+                    <FontAwesomeIcon
+                        icon={faTwitter}
+                        size="lg"
+                    />
+                    </a>
+                    <a href="https://www.facebook.com/restart.asso/">
+                    <FontAwesomeIcon
+                        icon={faFacebook}
+                        size="lg"
+                    />
+                    </a>
+                    <a href="mailto:hello@restart-asso.org">
+                    <FontAwesomeIcon
+                        icon={faEnvelope}
+                        size="lg"
+                    />
+                    </a>
+                </span>
+                <span>© Re:Start,  2019.</span>
+            </div>
+        </div>
+    </footer>
+)
--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../Header/header";
+import Footer from "../Footer/footer";
 
 export default ({children}) => (
     <div>
         <Header />
         {children}
+        <Footer />
     </div>
 );
