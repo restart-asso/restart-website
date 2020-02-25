ac74384443202f3d0c6d899b55061ad11c596c0d
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2020-02-25 19:02:26 +0100

    Add about component

---
+++ src/components/About/about.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./about.module.scss";
+
+const TeamMember = ({image, name, responsability}) => (
+    <div key={image} className={`has-text-centered ${styles.teamMember}`}>
+        <img src={image} alt=""/>
+        <div>
+            <h1 >{name}</h1>
+            <h2 >{responsability}</h2>
+        </div>
+    </div>
+);
+
+const About = ({aboutTitle, aboutSubtitle, aboutText, teamMembers}) => (
+    <div className={styles.wrapper}>
+        <div className={`container ${styles.aboutContainer}`}>
+            <div className="columns has-text-centered">
+                <div className="column">
+                    <div className={styles.columnContent}>
+                        <h1 className="title is-3">
+                            {aboutTitle}
+                        </h1>
+                        <h2 className="subtitle is-5">
+                            {aboutSubtitle}
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div className="columns" style={{ marginTop: 20}}>
+                <div className="column">
+                    <div className={styles.columnContent}>
+                        <p>{aboutText}</p>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="columns">
+                        {teamMembers.map( (member, index ) => {                    
+                            if (index !== (teamMembers.length-1)) {
+                                return (
+                                    <div className="column is-half">
+                                    <   TeamMember {...member} />
+                                    </div>
+                                );
+                            }
+                        })}
+                    </div>
+                    <div className="columns"> 
+                        <div className="column is-half is-offset-one-quarter">
+                            <TeamMember {...teamMembers[teamMembers.length-1]} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+About.propTypes = {
+    aboutTitle: PropTypes.string.isRequired,
+    aboutSubtitle: PropTypes.string.isRequired,
+    aboutText: PropTypes.string.isRequired,
+    teamMembers: PropTypes.shape({
+        image: PropTypes.string,
+        name: PropTypes.string,
+        responsability: PropTypes.string
+    }),
+}
+
+export default About; 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,45 @@ import React from "react";
 import Layout from "../components/Layout/layout";
 import ProgramContainer from "../components/ProgramContainer/programContainer"
 import Hero from "../components/Hero/hero"
+import About from "../components/About/about"
 import '../assets/styles.scss';
 
 const heroData = {
     slogan: "Dismantling Digital Divides",
-    missionStatement: "By provding inclusive digital educational & social support to newcomers in France",
+    missionStatement: "By providing inclusive digital educational & social support to newcomers in France",
     heroImage: "/img/hero.jpg"
 }
+
+const aboutData = {
+    aboutTitle: "About us",
+    aboutSubtitle: "Re:Start is an educational association that creates learning experiences and promotes inclusion in the digital economy",
+    aboutText: "The Re:Start Association started as a project in 2017 hosted by the Center for Research & Interdisciplinary (CRI). Re:Start began its activity opening a space to refugees and asylum seekers to learn highly demanded skills in order to promote their inclusion into the tech industry. Currently, we're continuing to co-create opportunities with newcomers in France; holding workshops and events to build digital, transversal and social skills, while being creative, playful and reflective."
+}
+
+const teamMembersData = [
+    {   
+        name: "Pau Fabregat",
+        image: "/img/pau-profile-pic.jpg",
+        responsability: "Coordination & Pedagogy"
+    },
+    {   
+        name: "Naiane Ribeiro",
+        image: "/img/naiane-profile-pic.png",
+        responsability: "Partnerships"
+    },
+    {   
+        name: "Kelly Gibbs",
+        image: "/img/kelly-profile-pic.jpg",
+        responsability: "Communication"
+    }
+]
 
 export default () => (
     <Layout>
     <div >
         <Hero {...heroData} /> 
         <ProgramContainer />
+        <About {...aboutData} teamMembers={teamMembersData}/>
     </div>
     </Layout>
 );
