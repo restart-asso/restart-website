a6b4d3d24408b340a2a95bda2ae6bba1b02990eb
Pau Fabregat Pappaterra <pau.fabregat.p@gmail.com>
2020-02-28 17:48:45 +0100

    Refactor header to handle mobile menu toggle

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
-import Image from "gatsby-image";
+import { Link } from "gatsby";
+import classNames from "classnames";
 import styles from "./header.module.scss";
+import logoImg from "../../assets/img/restart-icon.jpg"; 
 
 const ListLink = props => (
     <div>
@@ -9,50 +10,63 @@ const ListLink = props => (
     </div>
 );
 
-export default () => {
-    const logoImgData = useStaticQuery(graphql`
-        query {
-            logo: file(relativePath: { eq: "restart-icon.jpg" }) {
-                childImageSharp {
-                fluid(maxWidth: 100, quality: 100) {
-                    ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-        }
-    `)
-    console.log(logoImgData);
-    return (
-    <nav className={`navbar ${styles.navbar}`} role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-            <Link className={styles.logo} to="/">
-                <Image 
-                    fluid={logoImgData.logo.childImageSharp.fluid}
-                    alt="Re:Start logo" 
-                    style={{
-                        width: 57,
-                        height: 57
-                    }}
-                    />
-            </Link>
-            {/* change link tag by button tag */}
-            <a  role="button" 
-                className="navbar-burger burger" 
-                aria-label="menu" aria-expanded="false" 
-                data-target="navbarMenu">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-            </a>
-        </div>
-        
-        <div id="navbarMenu" className="navbar-menu">
-            <div className={`navbar-end ${styles.menu}`}>
-                <ListLink to="/">Home</ListLink>
-                <ListLink to="#programs">Programs</ListLink>
-                <ListLink to="#about">About</ListLink>
-            </div>
-        </div>
-    </nav>
-)};
+// TODO switch to a function using React Hooks
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    handleClick() {
+        this.setState(state => ({
+            isOpen: !state.isOpen
+        }))
+    };
+
+    render() {
+        const burgerClassName = classNames({
+            'navbar-burger': true,
+            'burger': true,
+            'is-active': this.state.isOpen
+        });
+
+        const navbarClassName = classNames({
+            'navbar-menu': true,
+            'is-active': this.state.isOpen
+        });
+        
+        return (
+            <nav className={`navbar ${styles.navbar}`} role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <Link className={styles.logo} to="/">
+                        <img src={logoImg} alt="Re:Start logo"/>
+                    </Link>
+
+                    <a  role="button" 
+                        className={burgerClassName}
+                        aria-label="menu" aria-expanded={this.state.isOpen} 
+                        data-target="navbarMenu"
+                        onClick={this.handleClick}
+                    >
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                    </a>
+                </div>
+                
+                <div id="navbarMenu" className={navbarClassName}>
+                    <div className={`navbar-end ${styles.menu}`}>
+                        <ListLink to="/">Home</ListLink>
+                        <ListLink to="#programs">Programs</ListLink>
+                        <ListLink to="#about">About</ListLink>
+                    </div>
+                </div>
+            </nav>
+            );
+    }
+};
+
+export default Header;
